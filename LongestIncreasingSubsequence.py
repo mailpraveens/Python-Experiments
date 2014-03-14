71f1bc5d981952f275500a2b62a67488b33e205b
praveen <praveen@altheasystems.com>
2014-03-14 22:34:28 +0530

    Longest increasing subsequence algo

---
+++ LongestIncreasingSubsequence.py
@@ -0,0 +1,31 @@
+#Finds a largest increasing subsequence in O(n^2) time
+#algorithm at http://www.algorithmist.com/index.php/Longest_Increasing_Subsequence
+def LongestSubsequence(array):
+  n=len(array)
+  q=[0]*n
+  p=[-1]*n # Contains all the previos elements to the increasing sequence
+ 
+  for i in range(n):
+    maxLen=0
+    for j in range(i):
+      if array[i]>array[j] :
+        if q[j]>maxLen :
+          maxLen=q[j]
+          p[i]=j
+
+    q[i]=maxLen+1
+ 
+  idx=q.index(max(q))
+  seq=[]
+  while(idx!=-1):
+    seq=[array[idx]]+seq
+    idx=p[idx]
+ 
+  return seq
+ 
+def main():
+  print(LongestSubsequence([4,2,6,1,9,0,11,7,12]))
+ 
+ 
+if __name__=='__main__':
+  main()
