09ee106827175f443276bcf4441ee7de7be01470
praveen <praveen@altheasystems.com>
2014-03-19 18:14:26 +0530

    Added method to find all paths.

--- Algorithm and solutions/findPathInAGraph.py
+++ Algorithm and solutions/findPathInAGraph.py
@@ -19,8 +19,25 @@ def find_path(graph, start, end, path=[]): # notice the default value for the fu
 			if newpath: return newpath
 	return None
 
+def find_all_paths(graph, start, end, path=[]): # notice the default value for the function Love python for that
+	path = path + [start] # start from the node
+	if(start == end):
+		return [path]
+	if not graph.has_key(start):
+		return None #invalid graph
+	paths = []
+	for node in graph[start]:
+		if node not in path:
+			newpaths = find_all_paths(graph,node,end,path)
+			for newpath in newpaths:
+				paths.append(newpath)
+	return paths
+
+
+
 def main():
 	print(find_path(graph,'A', 'D'))
+	print(find_all_paths(graph,'A', 'D'))
 
 
 if __name__ == '__main__':
